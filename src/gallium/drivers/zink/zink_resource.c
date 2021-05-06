1a1420d4972351cfb5e07da0f423988b39fde5f3
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-05-06 10:03:41 -0400

    assert

--- src/gallium/drivers/zink/zink_resource.c
+++ src/gallium/drivers/zink/zink_resource.c
@@ -1255,6 +1255,7 @@ zink_buffer_map(struct pipe_context *pctx,
          u_upload_alloc(mgr, 0, box->width + box->x,
                      screen->info.props.limits.minMemoryMapAlignment, &offset,
                      (struct pipe_resource **)&trans->staging_res, (void **)&ptr);
+         assert(trans->staging_res);
          res = zink_resource(trans->staging_res);
          trans->offset = offset + box->x;
          usage |= PIPE_MAP_UNSYNCHRONIZED;
