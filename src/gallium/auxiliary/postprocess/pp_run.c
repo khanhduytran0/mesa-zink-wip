1dbb275ca0684f3bae6e2f4c19017198fccea1c6
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-06-07 10:05:37 -0400

    aux/pp: respect gallium atom ordering

--- src/gallium/auxiliary/postprocess/pp_run.c
+++ src/gallium/auxiliary/postprocess/pp_run.c
@@ -136,12 +136,12 @@ pp_run(struct pp_queue_t *ppq, struct pipe_resource *in,
                         CSO_BIT_RENDER_CONDITION));
 
    /* set default state */
+   cso_set_geometry_shader_handle(cso, NULL);
+   cso_set_tesseval_shader_handle(cso, NULL);
+   cso_set_tessctrl_shader_handle(cso, NULL);
    cso_set_sample_mask(cso, ~0);
    cso_set_min_samples(cso, 1);
    cso_set_stream_outputs(cso, 0, NULL, NULL);
-   cso_set_tessctrl_shader_handle(cso, NULL);
-   cso_set_tesseval_shader_handle(cso, NULL);
-   cso_set_geometry_shader_handle(cso, NULL);
    cso_set_render_condition(cso, NULL, FALSE, 0);
 
    // Kept only for this frame.
